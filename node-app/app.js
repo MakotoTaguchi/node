c54ea4a8e11ce4980112efbe03d4886abfb54da1
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2021-12-23 02:01:45 +0900

    main

--- node-app/app.js
+++ node-app/app.js
@@ -1,8 +1,11 @@
 const http = require("http");
 const fs = require("fs");
 const ejs = require("ejs");
+const url = require("url");
 
 const index_page = fs.readFileSync("./index.ejs", "utf-8");
+const other_page = fs.readFileSync("./other.ejs", "utf-8");
+const style_css = fs.readFileSync("./style.css", "utf-8");
 
 var server = http.createServer(getFromClient);
 
@@ -10,8 +13,38 @@ server.listen(3000);
 console.log("Server start!");
 
 function getFromClient(request, response) {
-  let content = ejs.render(index_page);
-  response.writeHead(200, { "Content-Type": "text/html" });
-  response.write(content);
-  response.end();
+  var url_parts = url.parse(request.url);
+  switch (url_parts.pathname) {
+    case "/":
+      var content = ejs.render(index_page, {
+        title: "Index",
+        content: "これはIndexページです。",
+      });
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(content);
+      response.end();
+      break;
+
+    case "/other":
+      var content = ejs.render(other_page, {
+        title: "Other",
+        content: "これは新しく用意したページです。",
+      });
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(content);
+      response.end();
+      break;
+
+    case "/style.css":
+      response.writeHead(200, { "Content-Type": "text/css" });
+      response.write(style_css);
+      response.end();
+      break;
+
+    default:
+      response.writeHead(200, { "Content-Type": "text/plain" });
+      response.write("no page...");
+      response.end();
+      break;
+  }
 }
